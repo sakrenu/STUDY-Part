d5d6ecefdc0e1d1dc7c5cd3a5930bb04cd70bb6d
sakrenu <renukasakshivpatil@gmail.com>
2024-12-05 15:35:23 +0530

    updated client side setup

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/')
+            .then(response => setMessage(response.data.message))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Study-Part</h1>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default App;
