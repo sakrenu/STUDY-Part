7b1cf7838fe54e59b3676f7924a0fc4c946bb0d4
sakrenu <renukasakshivpatil@gmail.com>
2024-12-07 01:21:49 +0530

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -24,24 +24,52 @@
 
 // export default App;
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// function App() {
+//     const [message, setMessage] = useState('');
+
+//     useEffect(() => {
+//         axios.get('http://127.0.0.1:5000/')
+//             .then(response => setMessage(response.data.message))
+//             .catch(error => console.error(error));
+//     }, []);
+
+//     return (
+//         <div>
+//             <h1>Study-Part</h1>
+//             <p>{message}</p>
+//         </div>
+//     );
+// }
+
+// export default App;
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-    const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        axios.get('http://127.0.0.1:5000/')
-            .then(response => setMessage(response.data.message))
-            .catch(error => console.error(error));
-    }, []);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/")
+      .then((response) => setMessage(response.data.message))
+      .catch((error) => console.error(error));
+  }, []);
 
-    return (
-        <div>
-            <h1>Study-Part</h1>
-            <p>{message}</p>
-        </div>
-    );
+  return (
+    <div className="min-h-screen bg-gradient-shift flex items-center justify-center">
+      <div className="card-neon">
+        <h1 className="text-5xl font-extrabold text-neon-pink mb-4">
+          Study-Part
+        </h1>
+        <p className="text-lg text-neon-cyan">
+          {message || "Loading message from backend..."}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
