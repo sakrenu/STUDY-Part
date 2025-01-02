fc3006fbefc6e6899285501fcea533ce9a32c839
Abhin B <85185063+bcijo@users.noreply.github.com>
2025-01-02 15:53:40 +0530

    removed files and comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './components/Home';
-// import Login from './components/Login';
-// import Signup from './components/Signup';
-
-// const App = () => {
-//     return (
-//         <Router>
-//             <Routes>
-//                 <Route path="/" element={<Home />} />
-//                 <Route path="/login" element={<Login />} />
-//                 <Route path="/signup" element={<Signup />} />
-//             </Routes>
-//         </Router>
-//     );
-// };
-
-// export default App;
 // client/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
