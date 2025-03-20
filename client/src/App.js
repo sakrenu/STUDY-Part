85778c8eabbbe9687c588055c5f2391c66d99936
Abhin B <85185063+bcijo@users.noreply.github.com>
2025-03-21 01:00:36 +0530

    routing fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-// client/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
@@ -11,35 +10,24 @@ import QuizDashboard from './pages/students/QuizDashboard';
 import LandingPage from './components/LandingPage'; 
 import ManageStudents from './pages/teachers/ManageStudents';
 import LearningMode from './pages/students/LearningMode';
-import QuizCreation from './pages/teachers/QuizCreation';
-import PointSegmentation from './pages/teachers/PointSegmentation';
+import QuizTeachingDashboard from './pages/teachers/QuizTeachingDashboard';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* Landing Page */}
           <Route path="/" element={<LandingPage />} />
-
-          {/* Authentication Routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-
-          {/* Teacher Routes */}
           <Route path="/dashboard" element={<TeacherFeaturePage />} />
           <Route path="/dashboard/teaching" element={<TeachingMode />} />
           <Route path="/dashboard/manage-students" element={<ManageStudents />} />
-          <Route path="/dashboard/quiz-creation" element={<QuizCreation />} />
-          <Route path="/point-segmentation" element={<PointSegmentation />} />
-
-          {/* Student Routes */}
+          <Route path="/dashboard/quiz-mode" element={<QuizTeachingDashboard />} />
           <Route path="/student-dashboard" element={<StudentDashboard />} />
           <Route path="/learning-mode" element={<LearningMode />} />
-
-          {/* Student Quiz Routes */}
           <Route path="/student-dashboard/quiz-mode" element={<QuizDashboard />} />
-          <Route path="/student-dashboard/quiz-mode/:quizId" element={<QuizDashboard />} />
+          <Route path="/student-dashboard/quiz-mode/:quizId" element={<QuizMode />} />
           <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </div>
